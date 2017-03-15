bdfc6a71c4d7e24ee6bc3b852727a31d5ee79db0
Takuhiro Yoshida <takuhiro@users.noreply.github.com>
2017-03-16 00:03:47 +0900

    Connect saveButton to MealViewController.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -14,6 +14,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     @IBOutlet weak var nameTextFIeld: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+        This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+        or constructed as part of addin a new meal.
+     */
+    var meal: Meal?
     
     override func viewDidLoad() {
         super.viewDidLoad()
