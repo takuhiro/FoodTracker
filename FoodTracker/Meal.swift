deefbaa1b15e98d7b7945059c1e286a0ad165720
Takuhiro Yoshida <takuhiro@users.noreply.github.com>
2017-03-15 10:47:05 +0900

    Create unit test of class Meal and fix it's initializer.

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -20,10 +20,16 @@ class Meal {
     
     init?(name: String, photo: UIImage?, rating: Int) {
         
-        // Initialization should fail if there is no name or if the rating is negative
-        if name.isEmpty || rating < 0 {
-            return nil 
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
         }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
         // Initialize stored properties
         self.name = name
         self.photo = photo
